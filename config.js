c1e45716371d9b42f5bfac2bd082a53e97fe7a88
funice <ofunice@gmail.com>
2021-03-29 19:36:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const local = {
   port: 3000,
   host: '0.0.0.0',
-  mongoUrl: 'mongodb://localhost:27017/wxrobot',
+  mongoUrl: 'mongodb://admin:158200@cluster0.eegld.mongodb.net/Cluster0?retryWrites=true&w=majority',
   secret: '123456',
   tianApiKey:''
 }
@@ -9,7 +9,7 @@ const development = {
 }
 const production = {
   port: 8081,
-  mongoUrl:'mongodb://username:password@ip:port/wxrobot',
+  mongoUrl:'mongodb://admin:158200@cluster0.eegld.mongodb.net/Cluster0?retryWrites=true&w=majority',
 }
 let config = Object.assign(local, development)
 if (process.env.NODE_ENV == 'production') {
